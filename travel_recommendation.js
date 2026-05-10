386ee9efd5108553e91eb7c82961bd62181f402b
Chris Holland <sareuth@gmail.com>
2026-05-10 19:33:26 +0100

    a lot of work

---
+++ travel_recommendation.js
@@ -0,0 +1,74 @@
+
+const destinationInput = document.getElementById('destinationInput');
+const btnSearch = document.getElementById('btnSearch');
+const btnClear = document.getElementById('btnClear');
+const destinationDiv = document.getElementById('destinations');
+
+function cleanInput(input) {
+    const cleaned = input.trim().toLowerCase();
+
+    switch (cleaned) {
+        case 'country':
+            return 'cities';
+        case 'temple':
+            return 'temples';
+        case 'beach':
+            return 'beaches';
+    }
+
+    return cleaned;
+}
+
+function searchDestinations() {
+    const searchTerm = destinationInput.value.trim().toLowerCase();
+    if (searchTerm.length == 0) return;
+
+    
+    fetch('travel_recommendation_api.json')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let results;
+            switch (searchTerm) {
+                case 'beach':
+                case 'beaches':
+                    results = data['beaches'];
+                    break;
+                case 'temple':
+                case 'temples':
+                    results = data['temples'];
+                    break;
+                case 'country':
+                case 'countries':
+                    results = data['countries']
+                    break;
+            }
+
+            if (results) {
+                let inner = '<div class="results-panel"><h2>Results</h2>';
+                results.forEach(result => {
+                    inner += '<div class="destination-card">';
+                    inner += `<img src="${result.imageUrl}" alt="${result.name}"></img>`;
+                    inner += `<h3>${result.name}</h3>`;
+                    inner += `<p>${result.description}</p>`;
+                    inner += '</div>';
+                });
+                inner += '</div>';
+                destinationDiv.innerHTML = inner;
+            } else {
+                destinationDiv.innerHTML = '<div class="results-panel"><h2>No locations found</h2></div>';
+            }
+        });
+
+
+
+}
+
+
+function clearSearch() {
+    destinationDiv.innerHTML = '';
+    destinationInput.value = '';
+}
+
+btnSearch.addEventListener('click', searchDestinations);
+btnClear.addEventListener('click', clearSearch);
